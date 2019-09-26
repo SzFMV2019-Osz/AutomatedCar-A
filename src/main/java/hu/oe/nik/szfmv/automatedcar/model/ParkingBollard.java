f166ecfd4a0e71237d9eed4873a286bb4a83472b
Kovács Sándor <kovacss2@wildom.com>
2019-09-26 17:29:07 +0200

    #17 - parking bollard objektum is lesz majd később

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/ParkingBollard.java
@@ -0,0 +1,10 @@
+package hu.oe.nik.szfmv.automatedcar.model;
+
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.IStatic;
+
+public class ParkingBollard extends WorldObject implements IStatic {
+
+    public ParkingBollard() {
+        this.imageFileName = "bollard";
+    }
+}
