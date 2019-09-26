4de74ce8f3ba400890605b62eb4b40f83b24958d
Kovács Sándor <kovacss2@wildom.com>
2019-09-26 20:42:32 +0200

    #38 - modellek bővítése interfészekkel

--- src/main/java/hu/oe/nik/szfmv/automatedcar/model/ParkingSpace.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/ParkingSpace.java
@@ -1,6 +1,7 @@
 package hu.oe.nik.szfmv.automatedcar.model;
 
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.IBackground;
 import hu.oe.nik.szfmv.automatedcar.model.interfaces.IStatic;
 
-public class ParkingSpace extends WorldObject implements IStatic {
+public class ParkingSpace extends WorldObject implements IStatic, IBackground {
 }
