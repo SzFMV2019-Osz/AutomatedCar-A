200744b8356d7e8722c51ebb085f35a702cd7d62
deseatnikov <deseatnikov.daniel@gmail.com>
2019-09-26 10:57:12 +0200

    WorldManager skeleton a4-nek

--- src/main/java/hu/oe/nik/szfmv/automatedcar/model/managers/WorldManager.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/managers/WorldManager.java
@@ -1,7 +1,10 @@
 package hu.oe.nik.szfmv.automatedcar.model.managers;
 
 import hu.oe.nik.szfmv.automatedcar.model.World;
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.IObject;
 import hu.oe.nik.szfmv.automatedcar.model.interfaces.IWorld;
+
+import java.util.List;
 
 public class WorldManager {
 
@@ -9,9 +12,9 @@ public class WorldManager {
 
 
     /**
-     * Initializes the world from a filepath
-     * @param filepath Path of the world file (eg.: test_world.xml)
-     * @return Return an IWorld object
+     * Inicializálja a jelenlegi világot egy xml file alapján
+     * @param filepath Neve a világ fájlnak (pl.: test_world.xml)
+     * @return Egy IWorld-el tér vissza
      */
     public IWorld initWorld(String filepath) {
         // TODO
@@ -19,8 +22,8 @@ public class WorldManager {
     }
 
     /**
-     * Returns the current world if it's been initialized
-     * @return IWorld of the current world
+     * Visszaadja a jelenlegi világot, ha már inicializálva lett
+     * @return Jelenlegi világ IWorld-ként
      */
     public IWorld getWorld() {
         // TODO
@@ -29,7 +32,23 @@ public class WorldManager {
     }
 
     /**
-     * Throws an error if the initWorld hasn't been called yet
+     * Visszaadja a világban levő összes objektumot
+     * @return IObject lista amiben minden a világban levő object benne van
+     */
+    public List<IObject> getAllObjects() {
+        // TODO
+        validate();
+        return null;
+    }
+
+    public List<IObject> getAllObjectsInTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        // TODO
+        validate();
+        return null;
+    }
+
+    /**
+     * Hibát dob, ha az initWorld nem volt még meghívva.
      */
     private void validate() {
     }
