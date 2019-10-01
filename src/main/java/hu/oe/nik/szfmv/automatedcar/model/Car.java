30cefb31f9ad9426f40b057eb013cf4739bef98b
kaptast <kaptas.tamas@gmail.com>
2019-10-01 22:12:51 +0200

    #52 - Car initShape és Z index beállítása

--- src/main/java/hu/oe/nik/szfmv/automatedcar/model/Car.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/Car.java
@@ -3,6 +3,9 @@ package hu.oe.nik.szfmv.automatedcar.model;
 import hu.oe.nik.szfmv.automatedcar.model.interfaces.ICrashable;
 import hu.oe.nik.szfmv.automatedcar.model.interfaces.IDynamic;
 
+import java.awt.Shape;
+import java.awt.Rectangle;
+
 /**
  * Autó alaposztály.
  */
@@ -18,10 +21,21 @@ public class Car extends WorldObject implements ICrashable, IDynamic {
      */
     public Car(int x, int y, String imageFileName) {
         super(x, y, imageFileName);
+        this.position.setZ(1);
     }
 
     public double getWeight() {
         // TODO
         return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initShape(){
+        int x = 0 - (width / 2 - 6);
+        int y = 0 - (height / 2 - 1);
+        this.polygon = new Rectangle(x, y, this.width - 12, this.height - 2);
     }
 }
