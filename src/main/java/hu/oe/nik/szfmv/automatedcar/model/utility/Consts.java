cc48bbbe3fbb8691492d98595ef371041498c508
Kovács Sándor <kovacss2@wildom.com>
2019-10-02 21:52:36 +0200

    #51 - konstansok reference pontok beolvasásához

--- src/main/java/hu/oe/nik/szfmv/automatedcar/model/utility/Consts.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/utility/Consts.java
@@ -15,13 +15,16 @@ public final class Consts {
     public static final String RES_IDENTIFIER_CAR = "car";
     public static final String RES_IDENTIFIER_BOLLARD = "parking_bollard";
     public static final String XML_ATTRIBUTE_TYPE = "type";
+    public static final String XML_ATTRIBUTE_NAME = "name";
+    public static final String XML_ATTRIBUTE_X = "x";
+    public static final String XML_ATTRIBUTE_Y = "y";
 
     // --- Errors
 
     public static final String ERROR_COULDNT_LOAD_IMG_FILE = "Nem sikerült a képfájl betöltése!";
     public static final String ERROR_IN_PROCESSING = "Hiba lépett fel feldolgozás közben!";
     public static final String ERROR_FILE_LIKELY_DOESNT_EXIST = "Valószínűleg nem létezik a fájl!";
-    public static final String ERROR_IN_XML_PARSING = "Az XML parse-olása közben hiba lépett fel!";
+    public static final String ERROR_IN_XML_PARSING = "A {0} XML parse-olása közben hiba lépett fel!";
 
     // --- Messages
 
