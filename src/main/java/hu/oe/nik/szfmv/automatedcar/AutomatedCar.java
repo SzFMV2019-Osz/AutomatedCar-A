f3b33548ee6d5e00f819eed76da1ebea4c8477f3
kaptast <kaptas.tamas@gmail.com>
2019-11-02 20:04:44 +0100

    Collision

--- src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
@@ -1,8 +1,11 @@
 package hu.oe.nik.szfmv.automatedcar;
 
 import hu.oe.nik.szfmv.automatedcar.model.Car;
+import hu.oe.nik.szfmv.automatedcar.model.Position;
 import hu.oe.nik.szfmv.automatedcar.model.RoadSensor;
 import hu.oe.nik.szfmv.automatedcar.model.SignSensor;
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.IObject;
+import hu.oe.nik.szfmv.automatedcar.model.managers.WorldManager;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.Driver;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.Powertrain;
 import hu.oe.nik.szfmv.automatedcar.virtualfunctionbus.VirtualFunctionBus;
@@ -11,6 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.awt.geom.AffineTransform;
+import java.util.List;
 
 public class AutomatedCar extends Car {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -23,35 +27,35 @@ public class AutomatedCar extends Car {
     public AutomatedCar(int x, int y, String imageFileName) {
         super(x, y, imageFileName);
 
-        new Driver(virtualFunctionBus);
-        pt = new Powertrain(virtualFunctionBus, REFRESH_RATE, x, y, (float)getRotation(), getHeight());
+        new Driver(this.virtualFunctionBus);
+        this.pt = new Powertrain(this.virtualFunctionBus, REFRESH_RATE, x, y, (float) this.getRotation(), this.getHeight());
 
-        roadSensor = new RoadSensor();
-        roadSensor.setPos(getPosX(), getPosY());
+        this.roadSensor = new RoadSensor();
+        this.roadSensor.setPos(this.getPosX(), this.getPosY());
 
-        signSensor = new SignSensor();
-        signSensor.setPos(getPosX(), getPosY());
+        this.signSensor = new SignSensor();
+        this.signSensor.setPos(this.getPosX(), this.getPosY());
     }
 
     public void drive() {
-        virtualFunctionBus.loop();
-        calculatePositionAndOrientation();
+        this.virtualFunctionBus.loop();
+        this.calculatePositionAndOrientation();
     }
 
     public VirtualFunctionBus getVirtualFunctionBus() {
-        return virtualFunctionBus;
+        return this.virtualFunctionBus;
     }
 
     private void calculatePositionAndOrientation() {
-        var movingVector = virtualFunctionBus.powertrainPacket.getMovingVector();
-        this.setPosX(this.getPosX() + (int)movingVector.getX());
-        this.setPosY(this.getPosY() + (int)movingVector.getY());
-        setRotation(pt.getAutoSzoge());
+        var movingVector = this.virtualFunctionBus.powertrainPacket.getMovingVector();
+        this.setPosX(this.getPosX() + (int) movingVector.getX());
+        this.setPosY(this.getPosY() + (int) movingVector.getY());
+        this.setRotation(this.pt.getAutoSzoge());
 
-        roadSensor.setPos(roadSensor.getPosX() + (int) movingVector.getX(),
-                roadSensor.getPosY() + (int) movingVector.getY());
-        signSensor.setPos(signSensor.getPosX() + (int) movingVector.getX(),
-                signSensor.getPosY() + (int) movingVector.getY());
+        this.roadSensor.setPos(this.roadSensor.getPosX() + (int) movingVector.getX(),
+                this.roadSensor.getPosY() + (int) movingVector.getY());
+        this.signSensor.setPos(this.signSensor.getPosX() + (int) movingVector.getX(),
+                this.signSensor.getPosY() + (int) movingVector.getY());
     }
 
     @Override
@@ -62,5 +66,19 @@ public class AutomatedCar extends Car {
         shapeTransform.rotate(Math.toRadians(this.getRotation() + 90), this.getReferenceX(), this.getReferenceY());
 
         return shapeTransform;
+    }
+
+    public void checkCollisions(WorldManager manager, int offsetX, int offsetY) {
+        Position pointA = new Position(this.getPosX() - this.getReferenceX(), this.getPosY() - this.getReferenceY());
+        Position pointB = new Position(pointA.getX() + this.getWidth(), pointA.getY() + this.getHeight());
+
+        List<IObject> collidedObjects = manager.getAllCrashableObjectsInRectangle(pointA, pointB, offsetX, offsetY);
+
+        if (!collidedObjects.isEmpty()) {
+            for (IObject collidedObject : collidedObjects) {
+                Class c = collidedObject.getClass();
+                // (ICrashable) collidedObject.crashed();
+            }
+        }
     }
 }
