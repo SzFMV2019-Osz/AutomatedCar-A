ae12c40cfa9fa37754a4c0295d7be1fbe75e4711
kaptast <kaptas.tamas@gmail.com>
2019-11-06 00:32:36 +0100

    Combine sensors in Camera

--- src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
@@ -20,9 +20,6 @@ import java.util.List;
 public class AutomatedCar extends Car {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static final int CAMERA_RANGE = 80;
-    private static final int CAMERA_OFFSET = 40;
-
     private static final int REFRESH_RATE = 10;
     private Powertrain pt;
 
@@ -33,12 +30,7 @@ public class AutomatedCar extends Car {
 
         new Driver(this.virtualFunctionBus);
         this.pt = new Powertrain(this.virtualFunctionBus, REFRESH_RATE, x, y, (float) this.getRotation(), this.getHeight());
-
-        this.roadSensor = new RoadSensor();
-        this.roadSensor.setPos(this.getPosX(), this.getPosY() - CAMERA_OFFSET);
-
-        this.signSensor = new SignSensor();
-        this.signSensor.setPos(this.getPosX(), this.getPosY() - CAMERA_OFFSET);
+        this.camera = new Camera(x, y);
     }
 
     public void drive() {
@@ -56,10 +48,8 @@ public class AutomatedCar extends Car {
         this.setPosY(this.getPosY() + (int) movingVector.getY());
         this.setRotation(this.pt.getAutoSzoge());
 
-        this.roadSensor.setPos(this.roadSensor.getPosX() + (int) movingVector.getX(),
-                this.roadSensor.getPosY() + (int) movingVector.getY());
-        this.signSensor.setPos(this.signSensor.getPosX() + (int) movingVector.getX(),
-                this.signSensor.getPosY() + (int) movingVector.getY());
+        this.camera.setPos(this.camera.getPosX() + (int) movingVector.getX(),
+                this.camera.getPosY() + (int) movingVector.getY());
     }
 
     @Override
@@ -87,45 +77,6 @@ public class AutomatedCar extends Car {
     }
 
     public Shape checkCamera(WorldManager manager, int offsetX, int offsetY) {
-        Position pointA = new Position(this.roadSensor.getPosX() + offsetX, this.roadSensor.getPosY() + offsetY);
-        Position pointB = this.generateTriangleLeftPoint(this.roadSensor, offsetX, offsetY);
-        Position pointC = this.generateTriangleRightPoint(this.roadSensor, offsetX, offsetY);
-
-        AffineTransform transform = new AffineTransform();
-        //transform.rotate(Math.toRadians(this.getRotation() + 90), pointA.getX(), pointA.getY()); //TODO: remove +90 after TeamA3 merge
-        transform.rotate(Math.toRadians(this.getRotation() + 90), this.getPosX() + offsetX, this.getPosY() + offsetY); //TODO: remove +90 after TeamA3 merge
-        Shape cameraTriangle = transform.createTransformedShape(ModelCommonUtil.generateTriangle(pointA, pointB, pointC));
-
-        List<IObject> signs = this.signSensor.getAllSensedRelevantObjects(manager, cameraTriangle, offsetX, offsetY);
-        List<IObject> roads = this.roadSensor.getAllSensedRelevantObjects(manager, cameraTriangle, offsetX, offsetY);
-
-        if (!signs.isEmpty()) {
-            System.out.println("Sings found");
-        }
-
-        if (!roads.isEmpty()) {
-            System.out.println("Roads found");
-        }
-
-        return cameraTriangle;
-    }
-
-    private Position generateTriangleLeftPoint(Sensor sensor, int offsetX, int offsetY) {
-        int rangeModifier = this.CAMERA_RANGE * Consts.PIXEL_PER_METERS / 10; // TODO: remove 100
-        Position point = new Position(sensor.getPosX() + offsetX - this.calculateTriangleSide(rangeModifier, 30), sensor.getPosY() + offsetY - rangeModifier);
-        //TODO: rotate by car rotation
-        return point;
-    }
-
-    private Position generateTriangleRightPoint(Sensor sensor, int offsetX, int offsetY) {
-        int rangeModifier = this.CAMERA_RANGE * Consts.PIXEL_PER_METERS / 10; // TODO: remove 100
-        Position point = new Position(sensor.getPosX() + offsetX + this.calculateTriangleSide(rangeModifier, 30), sensor.getPosY() + offsetY - rangeModifier);
-        //TODO: rotate by car rotation
-        return point;
-    }
-
-    private int calculateTriangleSide(int b, double angle) {
-        double angleInRad = Math.toRadians(angle);
-        return (int) (b * Math.tan(angleInRad));
+        return this.camera.loop(manager, this, offsetX, offsetY, this.getRotation());
     }
 }
---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/Camera.java
@@ -0,0 +1,92 @@
+package hu.oe.nik.szfmv.automatedcar.model;
+
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.IObject;
+import hu.oe.nik.szfmv.automatedcar.model.interfaces.ISensor;
+import hu.oe.nik.szfmv.automatedcar.model.managers.WorldManager;
+import hu.oe.nik.szfmv.automatedcar.model.utility.Consts;
+import hu.oe.nik.szfmv.automatedcar.model.utility.ModelCommonUtil;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Camera {
+    private static final int CAMERA_RANGE = 80;
+    private static final int CAMERA_OFFSET = 40;
+
+    public Position pos = new Position();
+    private List<ISensor> sensorList;
+
+    public Camera(int x, int y) {
+        this.sensorList = new ArrayList<>();
+
+
+        RoadSensor roadSensor = new RoadSensor();
+        roadSensor.setPos(x, y - CAMERA_OFFSET);
+        this.sensorList.add(roadSensor);
+
+        SignSensor signSensor = new SignSensor();
+        signSensor.setPos(x, y - CAMERA_OFFSET);
+        this.sensorList.add(signSensor);
+
+        this.pos = new Position(x, y - CAMERA_OFFSET);
+    }
+
+    public int getPosX() {
+        return this.pos.getX();
+    }
+
+    public int getPosY() {
+        return this.pos.getY();
+    }
+
+    public void setPos(int x, int y) {
+        this.pos.setX(x);
+        this.pos.setY(y);
+    }
+
+    public List<ISensor> getSensors() {
+        return this.sensorList;
+    }
+
+
+    public Shape loop(WorldManager manager, IObject parent, int offsetX, int offsetY, double rotation) {
+        Position pointA = new Position(this.getPosX() + offsetX, this.getPosY() + offsetY);
+        Position pointB = this.generateTriangleLeftPoint(offsetX, offsetY);
+        Position pointC = this.generateTriangleRightPoint(offsetX, offsetY);
+
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(Math.toRadians(rotation + 90), parent.getPosX() + offsetX, parent.getPosY() + offsetY); //TODO: remove +90 after TeamA3 merge
+        Shape cameraTriangle = transform.createTransformedShape(ModelCommonUtil.generateTriangle(pointA, pointB, pointC));
+
+        for (ISensor sensor : this.sensorList) {
+            List<IObject> list = sensor.getAllSensedRelevantObjects(manager, cameraTriangle, offsetX, offsetY);
+
+            if (!list.isEmpty()) {
+                System.out.println("Objects found");
+            }
+        }
+
+        return cameraTriangle;
+    }
+
+    private Position generateTriangleLeftPoint(int offsetX, int offsetY) {
+        int rangeModifier = this.CAMERA_RANGE * Consts.PIXEL_PER_METERS / 10; // TODO: remove range reduction
+        Position point = new Position(this.getPosX() + offsetX - this.calculateTriangleSide(rangeModifier, 30), this.getPosY() + offsetY - rangeModifier);
+        //TODO: rotate by car rotation
+        return point;
+    }
+
+    private Position generateTriangleRightPoint(int offsetX, int offsetY) {
+        int rangeModifier = this.CAMERA_RANGE * Consts.PIXEL_PER_METERS / 10; // TODO: remove range reduction
+        Position point = new Position(this.getPosX() + offsetX + this.calculateTriangleSide(rangeModifier, 30), this.getPosY() + offsetY - rangeModifier);
+        //TODO: rotate by car rotation
+        return point;
+    }
+
+    private int calculateTriangleSide(int b, double angle) {
+        double angleInRad = Math.toRadians(angle);
+        return (int) (b * Math.tan(angleInRad));
+    }
+}
